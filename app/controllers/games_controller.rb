9744caa795fac0c25bff1be551eba7ed43052817
Hoon Lee <yl.hoony@gmail.com>
2018-04-24 10:50:52 -0700

    updated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,31 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+  def index
+    game = Game.all
+    render json: game, status: 201    
+  end
+
+  def create
+    game = Game.create(game_params)
+    render json: game, status: 201
+  end
+
+  def show
+    game = Game.find(params[:id])
+    render json: game, status: 201
+  end
+
+  def update
+    game = Game.find(params[:id])
+    game.update(game_params)
+    render json: game, status: 201
+  end
+
+  private
+
+  def game_params
+    params.permit(state:[])    
+  end
+
 end
